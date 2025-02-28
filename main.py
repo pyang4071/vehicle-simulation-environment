2232070da1ba54d833733c213263d6a263335bc3
pyang4071 <paddyyang4071@gmail.com>
2025-02-28 18:14:09 -0500

    add better description and default value for flags

--- main.py
+++ main.py
@@ -3,15 +3,11 @@
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(prog = 'main.py',
-                                     description = 'parsing input/output')
-    parser.add_argument('-i', '--input')
-    parser.add_argument('-o', '--output')
+    parser = argparse.ArgumentParser(description = 'Simulates the movement of a vehicle in a 2D virtual enviroment with obstacles')
+    parser.add_argument('-i', '--input', default=argparse.SUPPRESS)
+    parser.add_argument('-o', '--output', default=argparse.SUPPRESS)
     args = parser.parse_args()
     print(args)
 
-
 if __name__ == '__main__':
     main()
-
-
