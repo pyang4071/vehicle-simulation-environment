a93dbf3d44d79e80f9049100b1b267c746b94573
pyang4071 <paddyyang4071@gmail.com>
2025-02-28 15:30:10 -0500

    added input/output flags

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
-def main():
-    pass
+import argparse
 
-if __name__ = '__main__':
+def main():
+    parser = argparse.ArgumentParser(prog = 'main.py',
+                                     description = 'parsing input/output')
+    parser.add_argument('-i', '--input')
+    parser.add_argument('-o', '--output')
+    args = parser.parse_args()
+    print(args)
+
+
+if __name__ == '__main__':
     main()
 
 
