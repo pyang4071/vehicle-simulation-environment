a3d74a12e1d80b93f3be4132409e2e4afc54b8cd
pyang4071 <paddyyang4071@gmail.com>
2025-03-06 21:24:52 -0500

    updated argparse to use pathlib

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
+import pathlib
 
 def main():
     parser = argparse.ArgumentParser(description = 'Simulates the movement of a vehicle in a 2D virtual enviroment with obstacles')
-    parser.add_argument('-i', '--input', default=argparse.SUPPRESS, type = argparse.FileType('r', encoding = "utf-8"))
-    parser.add_argument('-o', '--output', default=argparse.SUPPRESS, type = argparse.FileType('w', encoding = "utf-8"))
+    parser.add_argument('-i', '--input', default=pathlib.Path('input.yml'), type = pathlib.Path)
+    parser.add_argument('-o', '--output', default=pathlib.Path('output.yml'), type = pathlib.Path)
     args = parser.parse_args()
     print(args)
 
