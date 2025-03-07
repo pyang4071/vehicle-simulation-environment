61a87b2c43d13b53ddf29efba3e9affeb36ec5d3
pyang4071 <paddyyang4071@gmail.com>
2025-03-06 20:38:09 -0500

    add argparse default to open passed in files

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description = 'Simulates the movement of a vehicle in a 2D virtual enviroment with obstacles')
-    parser.add_argument('-i', '--input', default=argparse.SUPPRESS)
-    parser.add_argument('-o', '--output', default=argparse.SUPPRESS)
+    parser.add_argument('-i', '--input', default=argparse.SUPPRESS, type = argparse.FileType('r', encoding = "utf-8"))
+    parser.add_argument('-o', '--output', default=argparse.SUPPRESS, type = argparse.FileType('w', encoding = "utf-8"))
     args = parser.parse_args()
     print(args)
 
